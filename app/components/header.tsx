28d04e4427f6d0d27c754982830b1bf621849df6
celinaLind <clind323@gmail.com>
2024-09-21 12:17:54 -0700

    add loading skeleton and no documents screen

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,18 @@
 
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { HeaderActions } from "./header-actions";
+import Link from "next/link";
 
 
 export function Header() {
     return <div className="bg-slate-900 py-4">
         <div className="container mx-auto flex justify-between items-center">
-            <div className="header-text text-2xl">BRAINIAX</div>
+            <div className="flex gap-12 item-center">
+            <Link href="/" className="header-text text-2xl">BRAINIAX</Link>
+            <nav>
+                <Link href='/' className="text-white hover:text-slate-300">Documents</Link>
+            </nav>
+            </div>
             <div className="flex gap-4 items-center">
                 <ModeToggle />
                 <HeaderActions />
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import { DocumentCard } from "./components/document-card";
 import CreateDocumentBtn from "./components/create-document-btn";
+import { Skeleton } from "@/components/ui/skeleton"
+import { Card } from "@/components/ui/card";
+import Image from 'next/image';
+
 
 
 
@@ -18,10 +22,37 @@ export default function Home() {
           <h1 className="text-4xl font-bold">My Documents</h1>
           <CreateDocumentBtn />
         </div>
+        {!documents && (
+          <div className="grid grid-cols-4 gap-4">
+         {new Array(8).fill(0).map((_, i) => (
+          <Card className="p-6 flex flex-col justify-between gap-2" >
+          <Skeleton className="h-[40px]" />
+          <Skeleton className="h-[40px]" />
+          <Skeleton className=" w-[80px] h-[40px]" />
+          </Card>
+          ))}
+          
+          </div>
+         )} 
+         {documents && documents.length === 0 && (
+        <div className="m-auto flex flex-col justify-center gap-6 items-center">
+          {/* since we are querying through ALL the documents this will automatically update as changes occur */}
+          <Image
+            src="/documents.svg"
+            width={200}
+            height={200}
+            alt="Girl waiting for document upload"
+          />
+          <h1 className="text-2xl font-bold">You have no documents</h1>
+          <CreateDocumentBtn />
+        </div>
+         )}
+        {documents && documents.length > 0 && (
         <div className="grid grid-cols-4 gap-4">
           {/* since we are querying through ALL the documents this will automatically update as changes occur */}
           {documents?.map((doc) => <DocumentCard document={doc} />)}
         </div>
+         )}
       </main>
     </div>
   );
